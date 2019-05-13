d61dcbf3b63266f304bde3bc666f08eec8477087
Wojciech Kłodkowski PGS <wklodkowski@pgs-soft.com>
2019-05-13 15:50:53 +0200

    Added controls

---
+++ WpfApp/WpfApp.Desktop/WpfApp.Desktop/ViewModels/Controls/Menu/MenuItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace WpfApp.Desktop.ViewModels.Controls.Menu
+{
+    public class MenuItemViewModel : ViewModelBase
+    {
+        public string Text { get; set; }
+    }
+}
--- WpfApp/WpfApp.Desktop/WpfApp.Desktop/ViewModels/Report/ReportViewModel.cs
+++ WpfApp/WpfApp.Desktop/WpfApp.Desktop/ViewModels/Report/ReportViewModel.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
-using WpfApp.Desktop.Common.Pages.Interfaces;
-using WpfApp.Desktop.Report.Consts;
+using WpfApp.Desktop.ViewModels.Report;
 
 namespace WpfApp.Desktop.Report.ViewModels
 {
-    public class ReportViewModel : ViewModelBase, IPageViewModel
+    public class ReportViewModel : ViewModelBase
     {
-        public string Name => ReportConsts.ReportMainPage;
+        public ReportMenuViewModel ReportMenu { get; set; }
     }
 }
