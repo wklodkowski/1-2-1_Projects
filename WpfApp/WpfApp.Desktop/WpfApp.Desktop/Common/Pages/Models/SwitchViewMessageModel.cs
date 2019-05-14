7bea41661ac3141c579fe6168db117d9f4e16cd5
Wojtek_Lap <wkx100@gmail.com>
2019-05-15 01:19:01 +0200

    refactor navigation

---
+++ WpfApp/WpfApp.Desktop/WpfApp.Desktop/Common/Pages/Models/SwitchViewMessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Desktop.Common.Pages.Enum;
+
+namespace WpfApp.Desktop.Common.Pages.Models
+{
+    public class SwitchViewMessageModel
+    {
+        public ApplicationPage Page { get; set; }
+    }
+}
--- WpfApp/WpfApp.Desktop/WpfApp.Desktop/ViewModels/MainViewModel.cs
+++ WpfApp/WpfApp.Desktop/WpfApp.Desktop/ViewModels/MainViewModel.cs
@@ -3,30 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
 using WpfApp.Desktop.Common.Pages.Enum;
+using WpfApp.Desktop.Common.Pages.Models;
 
 namespace WpfApp.Desktop.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Report;
-        public ViewModelBase CurrentPageViewModel { get; set; }
+
+        private FrameworkElement _contentControlView;
 
         public MainViewModel()
         {
-
+            Messenger.Default.Register<SwitchViewMessageModel>(this, (switchViewMessage) =>
+            {
+                SwitchView(switchViewMessage.Page);
+            });
         }
 
-        public void GoToPage(ApplicationPage page, ViewModelBase viewModel = null)
+        public FrameworkElement ContentControlView
         {
-            CurrentPageViewModel = viewModel;
-            var different = CurrentPage != page;
-            CurrentPage = page;
+            get => _contentControlView;
+            set
+            {
+                _contentControlView = value;
+                RaisePropertyChanged("ContentControlView");
+            }
+        }
 
-            if (!different)
-                RaisePropertyChanged(nameof(CurrentPage));
+        public void SwitchView(ApplicationPage page)
+        {
+            switch (page)
+            {
+                case ApplicationPage.Home:
+                    break;
+                case ApplicationPage.Report:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(page), page, null);
+            }
         }
     }
 }
