f379f9f3145dfbdf679ebab60b23f308862bbdf6
Wojtek_Lap <wkx100@gmail.com>
2019-06-13 18:28:54 +0200

    Added sample query

--- WpfApp/WpfApp.Desktop/WpfApp.BLL/Customers/Services/CustomerService.cs
+++ WpfApp/WpfApp.Desktop/WpfApp.BLL/Customers/Services/CustomerService.cs
@@ -7,6 +7,7 @@ using WpfApp.BLL.Customers.Mappers.Interfaces;
 using WpfApp.BLL.Customers.Models;
 using WpfApp.BLL.Customers.Services.Interfaces;
 using WpfApp.DAL;
+using WpfApp.DAL.Customers.Models;
 
 namespace WpfApp.BLL.Customers.Services
 {
@@ -32,6 +33,19 @@ namespace WpfApp.BLL.Customers.Services
         {
             var customersDb = _wpfAppContext.Customers.Where(c => c.LastName == lastName).ToList();
             return customersDb.Select(customer => _customerMapper.ToCustomerModel(customer)).ToList();
+        }
+
+        private List<Customer> GetCustomers(CustomerModel customerModel)
+        {
+            //  var query = someList.Where(a => (someCondition) ? a == "something" : true);
+            //var query = someList.Where(a => a == "something");
+            //if (condition)
+            //{
+            //    query = query.Where(b => b == "something else");
+            //}
+            //var result = query.ToList();
+
+            return null;
         }
     }
 }
