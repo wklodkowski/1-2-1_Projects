c55dc8beeddefce01322e9ef0ab2707a5291e790
Wojciech Kłodkowski PGS <wklodkowski@pgs-soft.com>
2019-07-11 12:16:27 +0200

    Added sql upskill project

---
+++ SqlUpskill/SqlUpskill/SqlUpskill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlUpskill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqlQueryService = new SqlQueryService();
+
+            var secondResult = sqlQueryService.GetSecondQueryResult();
+            Console.WriteLine(secondResult);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SqlUpskill/SqlUpskill/SqlUpskill/SqlQueryService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlUpskill
+{
+    public class SqlQueryService
+    {
+        private readonly AdventureWorks2017Entities _dbEntities;
+
+        public SqlQueryService()
+        {
+            _dbEntities = new AdventureWorks2017Entities();
+        }
+
+        public void GetFirstQueryResult()
+        {
+            
+        }
+
+        public dynamic GetSecondQueryResult()
+        {
+            var result = _dbEntities.StateProvinces
+                .Join(_dbEntities.Addresses,
+                    sp => sp.StateProvinceID,
+                    a => a.StateProvinceID,
+                    (province, address) => new {StateProvince = province, Address = address})
+                .Where(x => x.Address.StateProvinceID == null).Select(x => new { x.StateProvince.Name, x.StateProvince.StateProvinceID }).ToList();
+
+            return result;
+        }
+    }
+}
