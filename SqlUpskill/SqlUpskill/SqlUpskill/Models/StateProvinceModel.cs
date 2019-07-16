cd84d23ff1fbc73dc370c7c3634faa348bc981d5
Wojciech Kłodkowski PGS <wklodkowski@pgs-soft.com>
2019-07-16 17:07:28 +0200

    added linq upskill project

---
+++ SqlUpskill/SqlUpskill/SqlUpskill/Models/StateProvinceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlUpskill.Models
+{
+    public class StateProvinceModel
+    {
+        public int StateProvinceId { get; set; }
+        public string StateProvinceName { get; set; }
+    }
+}
--- SqlUpskill/SqlUpskill/SqlUpskill/SqlQueryService.cs
+++ SqlUpskill/SqlUpskill/SqlUpskill/SqlQueryService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SqlUpskill.Models;
 
 namespace SqlUpskill
 {
@@ -20,14 +21,34 @@ namespace SqlUpskill
             
         }
 
-        public dynamic GetSecondQueryResult()
+        public List<StateProvinceModel> GetSecondQueryResult()
         {
+            //var result = _dbEntities.StateProvinces.FirstOrDefault(x => x.Name == "Cantal");
+
+            //var result = _dbEntities.StateProvinces
+            //    .Join(_dbEntities.Addresses,
+            //        sp => sp.StateProvinceID,
+            //        a => a.StateProvinceID,
+            //        (province, address) => new { StateProvince = province, Address = address })
+            //    .Where(x => x.Address.StateProvinceID == null).Select(x => new { x.StateProvince.Name, x.StateProvince.StateProvinceID }).ToList();
+
+            //var result = _dbEntities.StateProvinces
+            //    .Join(_dbEntities.Addresses,
+            //        sp => sp.StateProvinceID,
+            //        a => a.StateProvinceID,
+            //        (province, address) => new { StateProvince = province, Address = address })
+            //    .Where(x => x.Address.StateProvinceID == null).Select(x => new StateProvinceModel
+            //    {
+            //        StateProvinceName = x.Address.AddressLine1,
+            //        StateProvinceId = x.StateProvince.StateProvinceID
+            //    }).ToList();
+
             var result = _dbEntities.StateProvinces
                 .Join(_dbEntities.Addresses,
                     sp => sp.StateProvinceID,
                     a => a.StateProvinceID,
-                    (province, address) => new {StateProvince = province, Address = address})
-                .Where(x => x.Address.StateProvinceID == null).Select(x => new { x.StateProvince.Name, x.StateProvince.StateProvinceID }).ToList();
+                    (province, address) => new StateProvinceModel{ StateProvinceName = province.Name, StateProvinceId = address.StateProvinceID})
+                .Where(x => x.StateProvinceId == null).ToList();
 
             return result;
         }
